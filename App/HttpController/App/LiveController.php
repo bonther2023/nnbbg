69544de495f2e0cd99cf057b103c3746e9d10129
bonther <qiuter2022@gmail.com>
2022-06-02 13:24:49 +0800

    6-2

--- App/HttpController/App/LiveController.php
+++ App/HttpController/App/LiveController.php
@@ -17,8 +17,6 @@ class LiveController extends BaseController
             $lists = $model->app();
             return $this->writeJson(0, encrypt_data($lists));
         } catch (\Throwable $e) {
-            write_log('Live-index:');
-            write_log($e->getMessage(), 4);
             return $this->writeJson(1, null, '请求失败，请稍后再试');
         }
     }

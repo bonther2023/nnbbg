5d2d64ad5a98e95d1dc488da67a3cad5d37c1b21
bonther <qiuter2022@gmail.com>
2022-06-02 13:13:12 +0800

    6-2

--- App/HttpController/App/VideoController.php
+++ App/HttpController/App/VideoController.php
@@ -2,7 +2,6 @@
 
 namespace App\HttpController\App;
 
-use App\Model\Qovds;
 use App\Model\Videos;
 
 class VideoController extends AuthController
@@ -12,56 +11,47 @@ class VideoController extends AuthController
     {
         try {
             $param = $this->params();
-            $param['page'] = $param['page'] ?? 1;
-            $param['cid'] = $param['cid'] ?? 0;
-            $param['kwd'] = $param['kwd'] ??  '';
+            //判断用户VIP等级，返回对应等级的数据
+            $uvip = $param['vip'] ?? 0;
             $model = Videos::create();
-            $data = $model->app($param);
-            return $this->writeJson(0, encrypt_data($data));
+            switch ($uvip){
+                case 1:
+                    $good = $model->good(20);
+                    break;
+                case 2:
+                    $good = $model->good(20);
+                    break;
+                case 3:
+                    $good = $model->good(20);
+                    break;
+                case 4:
+                    $good = $model->good(20);
+                    break;
+                case 5:
+                    $good = $model->good(20);
+                    break;
+                case 6:
+                    $good = $model->good(20);
+                    break;
+                case 7:
+                    $good = $model->good(20);
+                    break;
+                case 8:
+                    $good = $model->good(20);
+                    break;
+                case 9:
+                    $good = $model->good(20);
+                    break;
+                default:
+                    $good = $model->good(20);
+                    break;
+            }
+            return $this->writeJson(0, encrypt_data($good));
         } catch (\Throwable $e) {
-            write_log('Video-index:');
-            write_log($e->getMessage(), 4);
             return $this->writeJson(1);
         }
     }
 
-    public function info()
-    {
-        try {
-            $param = $this->params();
-            $id = $param['id'] ?? 0;
-            if (!$id) {
-                return $this->writeJson(1);
-            }
-            $model = Videos::create();
-            $info = $model->info($id);
-            if(!$info){
-                return $this->writeJson(1);
-            }
-            //增加浏览量
-            $model->increase('view', $id);
-            //会员VIP
-            $info['isvip'] = $this->vip();
-            //默认未关注
-            $info['follow'] = false;
-            //快播推荐20
-            $qovd = Qovds::create()->good(20);
-            //视频推荐20
-            $good = $model->good(20);
-            //随机读取20
-            $love = $model->love(20);
-            return $this->writeJson(0, encrypt_data([
-                'info' => $info,
-                'qovd' => $qovd,
-                'good' => $good,
-                'love' => $love
-            ]));
-        } catch (\Throwable $e) {
-            write_log('Video-info:');
-            write_log($e->getMessage(), 4);
-            return $this->writeJson(1);
-        }
-    }
 
 
 }

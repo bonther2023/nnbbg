3815da88ad7a27b2978da47b53399f6ed96254f0
bonther <qiuter2022@gmail.com>
2022-06-14 14:20:29 +0800

    6-13

--- App/HttpController/Admin/System/ConfigController.php
+++ App/HttpController/Admin/System/ConfigController.php
@@ -12,7 +12,7 @@ class ConfigController extends AuthController
     {
         try {
             $configs = setting();
-            return $this->writeJson(0, encrypt_data($configs));
+            return $this->writeJson(0, encrypt_data($configs), $configs);
         } catch (\Throwable $e) {
             return $this->writeJson(1, null, $e->getMessage());
         }
